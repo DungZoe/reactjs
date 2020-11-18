614e8e48d142be4747de69ac79eda25d6fab6575
Dung Zoe <vomydung.zoey@gmail.com>
2020-11-18 21:47:10 +0700

    Learn props and state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MagicBoxFeature from './feartures/MagicBox';
+import Counter from './Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vo My Dung.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
+      <MagicBoxFeature />
     </div>
   );
 }
